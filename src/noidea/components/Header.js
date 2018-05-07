ad51db55674444c640cd274be368d4a598ee1ae7
davidkvara <davekvaratskhelia@gmail.com>
2018-05-07 21:32:21 +0400

    responsive layout & added state to Header component for menu button

--- src/noidea/components/Header.js
+++ src/noidea/components/Header.js
@@ -2,19 +2,59 @@ import React from "react";
 import Nav from "./Nav";
 import Wrapper from "./Wrapper";
 
-const Header = props => {
-  return (
-    <header className="header">
-      <Wrapper>
-        <div className="header-container">
-          <h1 className="logo">
-            <a href="">TML</a>
-          </h1>
-          <Nav />
-        </div>
-      </Wrapper>
-    </header>
-  );
-};
+class Header extends React.Component {
+  state = { navIsVisible: true, width: window.innerWidth };
+
+  handleToggle = () => {
+    this.setState({ navIsVisible: !this.state.navIsVisible });
+  };
+
+  componentDidMount() {
+    // მემგონი ძვირი ჯდება..
+    // მაგრამ რეალურ სიტუაციაში საერთოდ არ მოგვიწევს ამ ფუნქციის გამოძახება
+    // პროდუქციაში მაინც არ გავუშვებდი
+    this.setState(state => {
+      if (window.innerWidth < 601) {
+        return { navIsVisible: false };
+      }
+    });
+    window.addEventListener("resize", this.updateDimensions);
+  }
+
+  updateDimensions = () => {
+    this.setState({
+      width: window.innerWidth
+    });
+    if (Number(this.state.width) < 600) {
+      this.setState({ navIsVisible: false });
+    } else {
+      this.setState({ navIsVisible: true });
+    }
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions);
+  }
+
+  render() {
+    return (
+      <header className="header">
+        <Wrapper>
+          <div className="header-container">
+            <h1 className="logo">
+              <a href="">TML</a>
+            </h1>
+            <div className="menu-btn">
+              <button onClick={this.handleToggle} className="menu-toggle">
+                menu
+              </button>
+            </div>
+            {this.state.navIsVisible && <Nav />}
+          </div>
+        </Wrapper>
+      </header>
+    );
+  }
+}
 
 export default Header;
